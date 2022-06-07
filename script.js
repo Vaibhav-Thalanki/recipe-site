967dcb2cd3793781fad060e58a38c3d1a7595829
Vaibhav-Thalanki <vaibhav.thalanki@gmail.com>
2022-06-07 11:56:59 +0530

    search functionality -partially complete

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+// FAVOURITES SECTION
+
+//auto load pizza and chicken in favourites
 $(document).ready(() => {
   ((global) => {
-    sendAPIreq("pizza");
-    sendAPIreq("chicken");
+    sendAPIreq("pizza", false, 0);
+    sendAPIreq("chicken", false, 0);
   })(window);
 });
+let APP_ID = "f9edf711";
+let API_KEY = "9eb288e3e07b2f96d536b0c5c8d4580a";
 let searchedFoodItems = ["pizza", "chicken"];
 let start = true;
+//search button for favourites
 $("#tile-1").click(() => {
   let food = $("#foodsearch").val();
   let flag = 0;
@@ -17,15 +23,13 @@ $("#tile-1").click(() => {
   }
   if (flag != 1) {
     if (start == true) searchedFoodItems = [];
-    sendAPIreq(food, start);
+    sendAPIreq(food, start, 0);
     searchedFoodItems.push(food);
   }
   start = false;
 });
-
-const sendAPIreq = async (food, start) => {
-  let APP_ID = "f9edf711";
-  let API_KEY = "9eb288e3e07b2f96d536b0c5c8d4580a";
+// API request to fetch for favorites as well as search section
+const sendAPIreq = async (food, start, val) => {
   if (start == true) {
     document.getElementById("apicards").innerHTML = "";
   }
@@ -47,9 +51,12 @@ const sendAPIreq = async (food, start) => {
   );
   let data = await response.json();
   console.log(data);
-  render(data);
+  if (val == 0) render(data); //for favourites
+  else {
+    renderforsearch(data);
+  }
 };
-
+// rendering favourites section
 const render = (data) => {
   let desc = data.hits[0].recipe.healthLabels;
   let desclen = Object.keys(desc).length;
@@ -82,3 +89,50 @@ ${data.hits[0].recipe.ingredientLines}
 </div>
   ` + existing;
 };
+
+//SEARCH ALL DISHES SECTION
+
+//search button
+$("#searchBtn").click(() => {
+  document.getElementById(
+    "apisearchcards"
+  ).innerHTML = `<section class="col-12  apitile">
+  <h2>Search Results</h2>
+</section>`;
+  var food = $("#searchvalue").val();
+  sendAPIreq(food, false, 1);
+});
+
+//rendering search section
+const renderforsearch = (data) => {
+  let html = document.getElementById("apisearchcards").innerHTML;
+  let n = data.hits.length;
+  for (let i = 0; i < n; i++) {
+    html += `
+    <div class="row d-flex justify-content-evenly pt-3" >
+    <div class='col-12 col-sm-6 col-lg-4 mb-3 renderedtiles'>
+    <div class='card container-fluid p-4'>
+  <img src='${data.hits[i].recipe.image}' class='card-img-top' alt='...'>
+  <div class='card-body'>
+    <h5 class='card-title'>${data.hits[i].recipe.label}</h5>
+    
+  </div>
+  </div>
+  </div>
+  <div class='col-12 col-sm-6 col-lg-7 mb-3 renderedIngredients'>
+  <div class='card container-fluid p-5'>
+  <h5>Ingredients</h5>
+  ${data.hits[i].recipe.ingredientLines}
+  <a href='${data.hits[i].recipe.url}' target='_blank' class='btn btn-primary recipeLink'>Get Recipe!</a>
+  <div class = 'btn btn-primary addFavo recipeLink mt-4' id='fav${i}'>Add to Favourites</div>
+  </div>
+  </div>
+  </div>
+    `;
+  }
+  document.getElementById("apisearchcards").innerHTML = html;
+};
+//add to favourites
+$(".addFavo").click((event) => {
+  console.log("brhu");
+});
